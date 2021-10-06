00ce9d2aac38bc3d3918ef7d878cd9e0d369b57a
Rohan Shivesh <rvss28@gmail.com>
2021-10-06 22:11:34 +0530

    conditional loading msg, handling fetch errors

--- src/Home.js
+++ src/Home.js
@@ -5,21 +5,34 @@ const Home = () => {
 
     // blogs is an array of blogs
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [myerror, setMyError] = useState(null);
 
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs').then(res => {
+            if (!res.ok) {
+                throw Error('could not fetch data');
+            }
             // res is not the output data but the response of the server
             return res.json();//this is async
         }).then((data) => {//here we get our data
             setBlogs(data);
+            setIsPending(false);
+            setMyError(null);//if the user hits reload after the error then reinitialise after no error is found
 
+        }).catch(err => {
+            console.log(err.message);
+            setMyError(err.message);
+            setIsPending(false);
         });
     }, []);//*here we pass an empty dependency, so that blogs are set only on initial render
 
     return (
         <div className="home">
             {/* Props are used to send data from one component to another */}
+            {isPending && <div> Loading... </div>}
+            {myerror && <div> {myerror} </div>}
             {blogs && <BlogList blogs={blogs} title="All My Blogs!" />}
             {//! what happens is that until the blogs data in filled in the var, the frontend gets rendered which is why in the bloglist, map method is called upon null value. thats why we put an AND condition
                 // ! blogs is on the left because after left value is null, right is not encountered i.e does not get rendered
