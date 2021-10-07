b1353b25b6926f86ffc820b0d8a06baa6ffd8574
Rohan Shivesh <rvss28@gmail.com>
2021-10-07 22:22:57 +0530

    add new blog form, submit to json server, redirect to home page. useHistory usage

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,13 +1,27 @@
 import { useParams } from "react-router";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
 
     const { id } = useParams();
+    const { data: blog, isPending, myerror } = useFetch('http://localhost:8000/blogs/' + id);
 
 
     return (
         <div className="blog-details">
-            <h2>Blog Details - {id}</h2>
+            {isPending && <div>Loading...</div>}
+            {myerror && <div>{myerror}</div>}
+            {
+                blog && (
+                    <article>
+                        <h2>{blog.title}</h2>
+                        <p>{blog.author}</p>
+                        <div>{blog.body}</div>
+
+                    </article>
+                )
+            }
+
         </div>
     );
 }
--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,53 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 const Create = () => {
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();//this does not refresh the page when we click the submit button
+        const newblog = { title, body, author };//no need for id, as json server does that automatically for us
+        setIsPending(true);
+        fetch('http://localhost:8000/blogs', {
+            //here we define the type of request
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(newblog)
+        }).then(() => {
+            console.log('added');
+            setIsPending(false);
+            // history.goBack(-1);//this more like acts like a back button
+            history.push('/');
+        });
+    }
+
+
     return (
         <div className="create">
             <h2>Add a new Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label >Blog title:</label>
+                <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} />
+                <label >Blog body:</label>
+                <textarea required value={body} onChange={(e) => setBody(e.target.value)}>
+
+                </textarea>
+                <label >Blog Author:</label>
+                <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button>Adding Blog</button>}
+                {/* <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p> */}
+            </form>
         </div>
     )
 }
